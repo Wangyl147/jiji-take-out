ae1ab74426973157e96822c70ce5519b06a88086
Wangyl147 <Wangyl147@bupt.edu.cn>
2023-02-22 15:47:07 +0800

    version 0.3

--- src/main/java/org/wangyl/reggie/controller/EmployeeController.java
+++ src/main/java/org/wangyl/reggie/controller/EmployeeController.java
@@ -26,13 +26,14 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * @param request
      * @param employee
      * @return
      */
-    @PostMapping("/login")// 因为是到/login的post请求
-    // 加入request是因为需要把员工id存入request的session中
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+    @PostMapping("/login")// 向/employee/login发起post请求
+    // 加入request是因为需要把员工id存入request的session中，如果不存session，后续跳转其他页面就不知道它有没有登录
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
 
         //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
@@ -40,26 +41,39 @@ public class EmployeeController {
 
         //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());//查询条件：等值查询
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());//查询条件：等值查询
         Employee emp = employeeService.getOne(queryWrapper);
 
         //3、如果没有查询到则返回登录失败结果
-        if (emp==null){
-            return R.error("登录失败");
+        if (emp == null) {
+            return R.error("用户不存在");
         }
 
         //4、密码比对，如果不一致则返回登录失败结果
-        if (!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("用户名或密码错误");
         }
 
         //5、查香员工状态，如果为已禁用状态，则返回员工已禁用结果
-        if(emp.getStatus()==0){
+        if (emp.getStatus() == 0) {
             return R.error("账号已禁用");
         }
 
         //6、登录成功，将员工id存入Session并返回登录成功结果
-        request.getSession().setAttribute("employee",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
+    }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理session中保存的当前登录的员工id
+        request.getSession().removeAttribute("employee");
+        //log.info("frgfrgrgergrg");
+        return R.success("退出成功");
     }
 }
